82fdb9e1758617253a733648db5faa782e8f6667
Murat Kasimov <iokasimov.m@gmail.com>
2018-09-21 18:00:21 +0300

    Add Functor and Foldable instances

--- Data/Route.hs
+++ Data/Route.hs
@@ -1,8 +1,11 @@
 module Data.Route where
 
-import "base" Text.Show (Show (show))
+import "base" Data.Foldable (Foldable (foldr))
+import "base" Data.Function (($))
+import "base" Data.Functor (Functor (fmap), (<$>))
 import "base" Data.Maybe (Maybe (Just, Nothing))
 import "base" Data.Semigroup ((<>))
+import "base" Text.Show (Show (show))
 import "free" Control.Comonad.Cofree (Cofree ((:<)))
 
 type Stack = Cofree Maybe
@@ -12,6 +15,8 @@ data Reference = Absolute | Relative
 newtype Route (reference :: Reference) resource
 	= Route { route :: Stack resource }
 
+-- | Relative routes are defined in direct order, absolute - in reverse
+
 instance Show resource => Show (Route Absolute resource) where
 	show (Route (x :< Just xs)) = show (Route @Absolute xs) <> " / " <> show x
 	show (Route (x :< Nothing)) = show x
@@ -19,3 +24,13 @@ instance Show resource => Show (Route Absolute resource) where
 instance Show resource => Show (Route Relative resource) where
 	show (Route (x :< Just xs)) = show x <> " / " <> show (Route @Relative xs)
 	show (Route (x :< Nothing)) = show x
+
+instance Functor (Route reference) where
+	fmap f (Route p) = Route $ f <$> p
+
+instance Foldable (Route Absolute) where
+	foldr f acc (Route (x :< Just xs)) = foldr f (f x acc) (Route @Absolute xs)
+	foldr f acc (Route (x :< Nothing)) = f x acc
+
+instance Foldable (Route Relative) where
+	foldr f acc (Route r) = foldr f acc r
