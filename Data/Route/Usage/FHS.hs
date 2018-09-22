983654d908f77a2f033ee8a612fb478ac4cf6a15
Murat Kasimov <iokasimov.m@gmail.com>
2018-09-22 19:17:59 +0300

    Fill FSH module with imports

--- Data/Route/Usage/FHS.hs
+++ Data/Route/Usage/FHS.hs
@@ -1,6 +1,18 @@
-module Data.Route.Usage.FHS (Path (..), current) where
+module Data.Route.Usage.FHS (Filepath (..), current) where
 
 -- | Filesystem Hierarchy Standard
+import "apart" Data.Apart.Structures.Stack (foldaway)
+import "base" Data.Function ((.), ($))
+import "base" Data.Functor ((<$>))
+import "base" Data.List (reverse, tail)
+import "base" Data.Maybe (maybe)
+import "base" Data.String (String)
+import "base" Text.Show (Show (show))
+import "base" System.IO (IO)
+import "directory" System.Directory (getCurrentDirectory)
+import "split" Data.List.Split (splitOn)
+
+import Data.Route (Route (Route), Reference (Absolute, Relative))
 
 data Filepath = Root
 	| Full (Route Absolute String)
@@ -8,8 +20,8 @@ data Filepath = Root
 	deriving Show
 
 -- | Get current working directory
-cwd :: IO Filepath
-cwd = parse <$> getCurrentDirectory where
+current :: IO Filepath
+current = parse <$> getCurrentDirectory where
 
 	parse :: String -> Filepath
 	parse "/" = Root
