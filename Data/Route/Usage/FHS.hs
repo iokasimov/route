51c00e8ec557a25dbb0eb68f079077c0c12969aa
Murat Kasimov <iokasimov.m@gmail.com>
2018-09-22 17:28:07 +0300

    Add datatype FHS and implement cwd command

--- Data/Route/Usage/FHS.hs
+++ Data/Route/Usage/FHS.hs
@@ -1,3 +1,13 @@
-module Data.Route.Usage.FHS where
+module Data.Route.Usage.FHS (Path (..), current) where
 
 -- | Filesystem Hierarchy Standard
+
+data Path = Root
+	| Full (Route Absolute String)
+	| Partial (Route Relative String)
+
+current :: IO Path
+current = getCurrentDirectory >>= \case
+	"/" -> pure Root
+	d -> pure . maybe Root (Full . Route)
+		. foldaway . splitOn "/" . tail $ d
